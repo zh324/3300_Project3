674d5a12e8f5455f26c97ecf6520ce65b3267490
Aruhan Liu <aruhanliu2@gmail.com>
2018-05-16 15:52:35 -0400

    trees added

--- js/buildTrees.js
+++ js/buildTrees.js
@@ -1,18 +1,91 @@
 function addTrees() {
-    var treesPosition = [
-      [-1000, -1000],
-      [-70, -110],
-      [10, -110],
-      [90, -110],
-      [170, -110],
-      [250, -110],
-      [320, -110],
-      [400, -110],
-      [480, -110],
-      [560, -110],
+    var treesPosition = [];
+    var x = 6000;
+    var y1 = -800;
+    var y2 = -1100;
+    //left main street trees
+    for (var i = 0; i < 40; i++) {
+      treesPosition.push([x - i * 300, y1]);
+      treesPosition.push([x - i * 300, y2])
+    }
 
+    //left sub street trees
+    for (var i = 0; i < 10; i++) {
+      treesPosition.push([-4500, -800 - i * 300]);
+      treesPosition.push([-4800, -800 - i * 300]);
 
-    ]
+      treesPosition.push([-3300, -800 - i * 300]);
+      treesPosition.push([-3600, -800 - i * 300]);
+
+      treesPosition.push([-2700, -800 - i * 300]);
+      treesPosition.push([-2400, -800 - i * 300]);
+
+      treesPosition.push([-1800, -800 - i * 300]);
+      treesPosition.push([-1500, -800 - i * 300]);
+
+      treesPosition.push([-600, -800 - i * 300]);
+      treesPosition.push([-300, -800 - i * 300]);
+
+      treesPosition.push([300, -800 - i * 300]);
+      treesPosition.push([600, -800 - i * 300]);
+
+      treesPosition.push([1500, -800 - i * 300]);
+      treesPosition.push([1800, -800 - i * 300]);
+
+      treesPosition.push([2400, -800 - i * 300]);
+      treesPosition.push([2700, -800 - i * 300]);
+
+      treesPosition.push([3300, -800 - i * 300]);
+      treesPosition.push([3600, -800 - i * 300]);
+
+      treesPosition.push([4200, -800 - i * 300]);
+      treesPosition.push([4500, -800 - i * 300]);
+      
+      treesPosition.push([5100, -800 - i * 300]);
+      treesPosition.push([5400, -800 - i * 300]);
+    }
+
+    //right main street trees
+    for (var i = 0; i < 40; i++) {
+      treesPosition.push([6000 - i * 300, 2300]);
+      treesPosition.push([6000 - i * 300, 2900])
+    }
+    
+    for (var i = 0; i < 6; i++) {
+      treesPosition.push([4500, 2300 - i * 300]);
+      treesPosition.push([5100, 2300 - i * 300]);
+
+      treesPosition.push([3600, 2300 - i * 300]);
+      treesPosition.push([3900, 2300 - i * 300]);
+
+      treesPosition.push([3000, 2300 - i * 300]);
+      treesPosition.push([2700, 2300 - i * 300]);
+
+      treesPosition.push([1800, 2300 - i * 300]);
+      treesPosition.push([2100, 2300 - i * 300]);
+
+      treesPosition.push([600, 2300 - i * 300]);
+      treesPosition.push([900, 2300 - i * 300]);
+
+      treesPosition.push([-300, 2300 - i * 300]);
+      treesPosition.push([-0, 2300 - i * 300]);
+
+      treesPosition.push([-1200, 2300 - i * 300]);
+      treesPosition.push([-900, 2300 - i * 300]);
+
+      treesPosition.push([-2400, 2300 - i * 300]);
+      treesPosition.push([-2100, 2300 - i * 300]);
+
+      treesPosition.push([-3300, 2300 - i * 300]);
+      treesPosition.push([-3000, 2300 - i * 300]);
+
+      treesPosition.push([-4200, 2300 - i * 300]);
+      treesPosition.push([-3900, 2300 - i * 300]);
+
+      treesPosition.push([-5100, 2300 - i * 300]);
+      treesPosition.push([-5400, 2300 - i * 300]);
+    }
+
     treesPosition.forEach(function(elem) {
       var x = elem[0],
         y = -200,
