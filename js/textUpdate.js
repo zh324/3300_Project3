a07bdfddbfc4e3d222a9aec9f2ade500dc433d4c
Aruhan Liu <aruhanliu2@gmail.com>
2018-05-16 23:41:53 -0400

    minor change

--- js/textUpdate.js
+++ js/textUpdate.js
@@ -3,13 +3,13 @@ function textInit(){
     nameInfo = d3.select('#buildingName').append('svg')
     .attr("id", "bar")
     .attr('width', "250")
-    .attr('height', "30");
+    .attr('height', "50");
     
     nameInfo.append("text")
     .attr("id","bName")
     .attr("x", 40)
     .attr("y", 20)
-    .style("font-size", "30px")
+    // .style("font-size", "30px")
     // .attr("class", "infoText")
 
     priceInfo = d3.select('#buildingPrice').append('svg')
@@ -33,17 +33,6 @@ function textInit(){
     .attr("x", 40)
     .attr("y", 20)
     .attr("class", "infoText")
-
-    // regionInfo = d3.select('#buildingRegion').append('svg')
-    // .attr("id", "bar")
-    // .attr('width', "250")
-    // .attr('height', "30");
-    
-    // regionInfo.append("text")
-    // .attr("id","bRegion")
-    // .attr("x", 40)
-    // .attr("y", 20)
-    // .attr("class", "infoText")
 
     addressInfo = d3.select('#buildingAddress').append('svg')
     .attr("id", "bar")
@@ -89,7 +78,7 @@ function textUpdate(name, price, rating, region, address, phone, website){
         .attr("id","bName")
         .attr("x", 40)
         .attr("y", 20)
-        .attr("class", "infoText")
+        // .attr("class", "infoText")
         .text(name);
 
         d3.selectAll("#bPrice")
@@ -110,12 +99,6 @@ function textUpdate(name, price, rating, region, address, phone, website){
             .attr("class", "infoText")
             .text("$");           
         }
-        // priceInfo.append("text")
-        // .attr("id","bPrice")
-        // .attr("x", 40)
-        // .attr("y", 20)
-        // .attr("class", "infoText")
-        // .text("Price:     " + price);
 
         d3.selectAll("#bRating")
         .style("display","none");
@@ -126,16 +109,6 @@ function textUpdate(name, price, rating, region, address, phone, website){
         .attr("y", 20)
         .attr("class", "infoText")
         .text("Rating:     " + rating);
-
-        // d3.selectAll("#bRegion")
-        // .style("display","none");
-
-        // regionInfo.append("text")
-        // .attr("id","bRegion")
-        // .attr("x", 40)
-        // .attr("y", 20)
-        // .attr("class", "infoText")
-        // .text("Region:     " + region);
 
         d3.selectAll("#bAddress")
         .style("display","none");
